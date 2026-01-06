ee6a6b2b2a21802f66091cdfa9db2ae348e07bdf
ShahriyarSaidi <sehriyarseidi@gmail.com>
2026-01-06 15:03:46 +0400

    saytda dəyişiklik etdim

--- js/music.js
+++ js/music.js
@@ -1,1 +1,36 @@
-document.addEventListener("DOMContentLoaded",(function(){const e=new Audio("music/We-Wish-You-chosic.com_.mp3");e.loop=!0,e.volume=.5;const n=document.querySelector(".mobile-menu-icon"),t=n.querySelector(".burger");let o=!1;const c=new(window.AudioContext||window.webkitAudioContext);c.createMediaElementSource(e).connect(c.destination);const i=()=>{"suspended"===c.state&&c.resume(),e.play().then((()=>{o=!0,t.innerHTML='<path d="M10 8H12V16H10V8ZM14 8H16V16H14V8Z"></path>'})).catch((()=>{console.log("Autoplay browser tərəfindən bloklandı, klikləmək lazım ola bilər.")}))};i(),n.addEventListener("click",(function(n){n.preventDefault(),o?(e.pause(),o=!1,t.innerHTML='<path d="M10 8L16 12L10 16V8Z"></path>'):i()}))}));
+document.addEventListener("DOMContentLoaded", function () {
+  const e = new Audio("music/We-Wish-You-chosic.com_.mp3");
+  e.loop = true;
+  e.volume = 0.5;
+
+  const n = document.querySelector(".mobile-menu-icon"),
+    t = n.querySelector(".burger");
+
+  let o = false;
+
+  const c = new (window.AudioContext || window.webkitAudioContext)();
+  c.createMediaElementSource(e).connect(c.destination);
+
+  const playMusic = () => {
+    if (c.state === "suspended") c.resume();
+    e.play().then(() => {
+      o = true;
+      t.innerHTML =
+        '<path d="M10 8H12V16H10V8ZM14 8H16V16H14V8Z"></path>';
+    });
+  };
+
+  // ❌ autoplay LƏĞV EDİLDİ
+  // playMusic();
+
+  n.addEventListener("click", function (n) {
+    n.preventDefault();
+    if (o) {
+      e.pause();
+      o = false;
+      t.innerHTML = '<path d="M10 8L16 12L10 16V8Z"></path>';
+    } else {
+      playMusic();
+    }
+  });
+});
