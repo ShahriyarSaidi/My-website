68d0e1def57df23eb0831b3b182f93f1c142104b
ShahriyarSaidi <sehriyarseidi@gmail.com>
2026-01-06 14:59:56 +0400

    saytda dəyişiklik etdim

--- js/lazy.js
+++ js/lazy.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll("img");
-    images.forEach(img => {
-      // Əgər artıq yoxdursa əlavə et
-      if (!img.hasAttribute("loading")) {
-        img.setAttribute("loading", "lazy");
-      }
-      if (!img.hasAttribute("decoding")) {
-        img.setAttribute("decoding", "async");
-      }
-    });
-  });
+document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll("img").forEach((t=>{t.hasAttribute("loading")||t.setAttribute("loading","lazy"),t.hasAttribute("decoding")||t.setAttribute("decoding","async")}))}));
